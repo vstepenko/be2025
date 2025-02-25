4c340cfa7414a1b1e2ec6c98b34469fd5bd4c712
vstep <vstepenko@alliancedigital.tech>
2025-02-25 09:36:39 +0200

    init BE project

---
+++ src/main/java/ua/edu/duan/be2025/Be2025Application.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.be2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Be2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Be2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/duan/be2025/Be2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.be2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Be2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
